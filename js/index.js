03ffe5adb62e25d7c4e8abba7cb8075d949ecea6
Pytniza <ip.web.so@gmail.com>
2019-01-03 11:33:53 +0300

    feat: add slider

--- js/index.js
+++ js/index.js
@@ -1,37 +1,30 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var slider = document.querySelector('.js_slider');
-    var slideCount = slider.querySelectorAll('.js_slide').length;
-    var dotContainer = slider.querySelector('.slider_navigation_dots');
-    var templateListItem = document.createElement('li');
-    
-    function handleEvents(e) {
-      if (e.type === 'before.lory.init') {
-        for (var i = 0, len = slideCount; i < len; i++) {
-          var clone = templateListItem.cloneNode();
-          dotContainer.appendChild(clone);
-        }
-        dotContainer.childNodes[0].classList.add('active');
-      }
-      if (e.type === 'after.lory.init') {
-        for (var i = 0, len = slideCount; i < len; i++) {
-          dotContainer.childNodes[i].addEventListener('click', function(e) {
-            lorySlider.slideTo(Array.prototype.indexOf.call(dotContainer.childNodes, e.target));
-          });
-        }
-      }
-      if (e.type === 'after.lory.slide') {
-        for (var i = 0, len = dotContainer.childNodes.length; i < len; i++) {
-          dotContainer.childNodes[i].classList.remove('active');
-        }
-        dotContainer.childNodes[e.detail.currentSlide - 1].classList.add('active');
-      }
-    }
-  
-    slider.addEventListener('before.lory.init', handleEvents);
-    slider.addEventListener('after.lory.init', handleEvents); 
-    slider.addEventListener('after.lory.slide', handleEvents); 
-  
-    var lorySlider = lory(slider, {
-      infinite: 1
-    });
-  });
+let quote_1 = document.getElementById('quote-1');
+let quote_2 = document.getElementById('quote-2');
+let quote_3 = document.getElementById('quote-3');
+let slide_1 = document.getElementById('slide-1');
+let slide_2 = document.getElementById('slide-2');
+let slide_3 = document.getElementById('slide-3');
+quote_1.onclick = function() {
+  slide_1.style.display = "inline";
+  slide_2.style.display = "none";
+  slide_3.style.display = "none";
+  quote_1.style.background = "#fff";
+  quote_2.style.background = "#424241";
+  quote_3.style.background = "#424241";
+}
+quote_2.onclick = function() {
+  slide_2.style.display = "inline";
+  slide_1.style.display = "none";
+  slide_3.style.display = "none";
+  quote_2.style.background = "#fff";
+  quote_1.style.background = "#424241";
+  quote_3.style.background = "#424241";
+}
+quote_3.onclick = function() {
+  slide_3.style.display = "inline";
+  slide_1.style.display = "none";
+  slide_2.style.display = "none";
+  quote_3.style.background = "#fff";
+  quote_2.style.background = "#424241";
+  quote_1.style.background = "#424241";
+}
